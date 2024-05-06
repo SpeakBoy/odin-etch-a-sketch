d1fbd6168615d095c59e8d277eeeb9a1fa210118
SpeakBoy <speakman@mcmaster.ca>
2024-05-06 18:02:40 -0400

    Add clear grid function

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let gridRows = 16;
-let gridCols = 16;
 const gridContainer = document.querySelector("div");
 
-function createGrid() {
+function createGrid(gridRows) {
     for (let i = 0; i < gridRows; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
         gridContainer.appendChild(gridRow);
-        for (let j = 0; j < gridCols; j++) {
+        for (let j = 0; j < gridRows; j++) {
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             gridRow.appendChild(gridSquare);
         }
     }
+}
+
+function clearGrid() {
+    gridContainer.innerHTML = "";
 }
