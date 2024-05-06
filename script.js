9682216d8248a08d927e70c872ff4188d89f0d1a
SpeakBoy <speakman@mcmaster.ca>
2024-05-06 19:37:15 -0400

    Add flexible dimensions and new grid function

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const gridContainer = document.querySelector("div");
+const gridContainer = document.querySelector(".grid-container");
+const newGridBtn = document.querySelector("button");
+
+newGridBtn.addEventListener("click", createNewGrid);
 
 function createGrid(gridRows) {
     for (let i = 0; i < gridRows; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
-        gridContainer.appendChild(gridRow);
         for (let j = 0; j < gridRows; j++) {
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             gridRow.appendChild(gridSquare);
-
             gridSquare.addEventListener("mouseover", () => {
-                gridSquare.classList.add("filled-square");
+                gridSquare.style.backgroundColor = "black";
             });
+            gridContainer.appendChild(gridRow);
         }
     }
 }
 
 function clearGrid() {
     gridContainer.innerHTML = "";
+}
+
+function createNewGrid() {
+    const gridRows = prompt(
+        "Please input the number of rows you would like your grid to contain (max 100):"
+    );
+
+    if (
+        !isNaN(gridRows) &&
+        gridRows % 1 === 0 &&
+        gridRows <= 100 &&
+        gridRows > 0
+    ) {
+        clearGrid();
+        createGrid(gridRows);
+    } else {
+        alert("Invalid input. Please try again.");
+    }
 }
